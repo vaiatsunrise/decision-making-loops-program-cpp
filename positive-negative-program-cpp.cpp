d443c71c00a2527679f7b12430f336f494b4f61d
Vaishnavi Verma <vaiatsunrise@gmail.com>
2025-07-25 09:27:46 +0530

    Add files via upload

---
+++ positive-negative-program-cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num > 0)
+        cout << "Positive number";
+    else if (num < 0)
+        cout << "Negative number";
+    else
+        cout << "Zero";
+
+    return 0;
+}
